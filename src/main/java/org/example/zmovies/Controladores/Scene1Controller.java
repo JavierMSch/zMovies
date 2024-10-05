57bc9a55881dc456cff9aecb1ad4d8ed983fe7f4
diegusho <diego.negrinramirez@gmail.com>
2024-10-05 11:50:11 -0300

    Agregada clase abstracta Controller, padre de Scene1Controller, Scene3Controller y Scene4Controller. Arregladas cositas.

--- src/main/java/org/example/zmovies/Controladores/Scene1Controller.java
+++ src/main/java/org/example/zmovies/Controladores/Scene1Controller.java
@@ -4,25 +4,16 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.MouseButton;
 import javafx.scene.layout.*;
-import org.example.zmovies.Modelos.VideoClub;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * El controlador de la escena de atención al cliente del video club. Permite rentar, devolver y recomendar películas,
  * ver las películas no devueltas y la película más rentada de cada género.
  */
-public class Scene1Controller {
-    private VideoClub videoClub;
-    @FXML
-    private Pane contentPane;
+public class Scene1Controller extends Controller {
     private Label titleLabel;
     private Label textLabel1;
     private Label textLabel2;
@@ -35,15 +26,6 @@ public class Scene1Controller {
     private Button cancelButton;
     private HBox horizontalLayout;
     private VBox verticalLayout;
-
-    /**
-     * Establece la instancia de VideoClub que se utilizará en este controlador.
-     *
-     * @param videoClub Instancia de VideoClub a asignar
-     */
-    public void setVideoClub(VideoClub videoClub) {
-        this.videoClub = videoClub;
-    }
 
     /**
      * Maneja el evento de hacer click en el botón "Rentar".
@@ -178,8 +160,9 @@ public class Scene1Controller {
      * @param event Evento de acción generado por el click
      * @throws IOException si hay un error al cambiar la escena
      */
+    @Override
     @FXML
-    private void onVolverClick(ActionEvent event) throws IOException {
+    protected void onSalirClick(ActionEvent event) throws IOException {
         SceneManager.switchScene("/fxml/scene0-view.fxml");
     }
 
@@ -494,25 +477,6 @@ public class Scene1Controller {
                 labels[2].setText("Costo Semanal: " + items[2]);
             }
         });
-    }
-
-    /**
-     * Restablece el panel de contenido a su estado predeterminado.
-     * Limpia el contenido actual del panel y muestra el icono de la aplicación en el centro del panel.
-     */
-    private void mostrarDefault() {
-        contentPane.getChildren().clear();
-
-        Image icon = new Image(Objects.requireNonNull(SceneManager.class.getResourceAsStream("/images/icon.png")));
-        ImageView iconItem = new ImageView(icon);
-        iconItem.setFitWidth(150);
-        iconItem.setFitHeight(150);
-        iconItem.setPreserveRatio(true);
-        double centerX = (contentPane.getWidth() - iconItem.getFitWidth()) / 2;
-        double centerY = (contentPane.getHeight() - iconItem.getFitHeight()) / 2;
-        iconItem.setLayoutX(centerX);
-        iconItem.setLayoutY(centerY);
-        contentPane.getChildren().add(iconItem);
     }
 
     /**
@@ -852,77 +816,5 @@ public class Scene1Controller {
 
         // Añade el panel de pila al panel de contenido
         contentPane.getChildren().add(stackPane);
-    }
-
-    /**
-     * Formatea un RUT (Rol Único Tributario) a un formato estándar.
-     *
-     * @param input RUT a formatear
-     * @return RUT formateado
-     */
-    private String formatoRut(String input) {
-        if (input == null) {
-            return "";
-        }
-        String lastChar = "";
-        if (input.endsWith("k") || input.endsWith("K")) {
-            lastChar = "K";
-        }
-        input = input.replaceAll("\\D", "");
-        input += lastChar;
-
-        if (input.length() < 8 || input.length() > 10) {
-            return input;
-        }
-
-        int offset = input.length() - 8;
-
-        String part1 = input.substring(0, 1 + offset);
-        String part2 = input.substring(1 + offset, 4 + offset);
-        String part3 = input.substring(4 + offset, 7 + offset);
-        String part4 = input.substring(7 + offset);
-        return part1 + "." + part2 + "." + part3 + "-" + part4;
-    }
-
-    /**
-     * Verifica si un campo de texto está vacío.
-     *
-     * @param textField Campo de texto a verificar
-     * @return true si el campo de texto está vacío, false en caso contrario
-     */
-    private boolean isFieldEmpty(TextField textField) {
-        return textField.getText() == null || textField.getText().trim().isEmpty();
-    }
-
-    /**
-     * Asigna un evento al TextField para que al presionar la tecla Enter se cambie el foco al Control recibido.
-     *
-     * @param currentField TextField actual
-     * @param nextControl Control al que se cambiará el foco
-     */
-    private void handleEnterKey(TextField currentField, Control nextControl) {
-        currentField.setOnKeyPressed(event -> {
-            if (Objects.requireNonNull(event.getCode()) == KeyCode.ENTER) {
-                if (nextControl instanceof TextField) {
-                    nextControl.requestFocus();
-                } else if (nextControl instanceof Button) {
-                    ((Button) nextControl).fire();
-                }
-            }
-        });
-    }
-
-    /**
-     * Asigna un evento al ListView para que al hacer doble click en un item se ejecute el evento del botón recibido.
-     *
-     * @param listView ListView en el que se hará doble click
-     * @param submitButton Button que se ejecutará al hacer doble click
-     */
-    private void handleDoubleClicked(ListView<String> listView, Button submitButton) {
-        listView.setOnMouseClicked(event -> {
-            if (event.getButton().equals(MouseButton.PRIMARY) && event.getClickCount() == 2) {
-                submitButton.fire();
-            }
-        });
     }
 }
